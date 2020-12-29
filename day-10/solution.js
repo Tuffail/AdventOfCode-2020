1a03c4fa544c12914c24b233774fa025804067fc
Tuffail Rahman <tuffailahmed159@gmail.com>
2020-12-29 03:31:58 +0000

    Day 10 complete

--- day-10/solution.js
+++ day-10/solution.js
@@ -1,6 +1,5 @@
 import { parseInput } from '../helpers/parsers';
 
-// eslint-disable-next-line import/prefer-default-export
 export const problem1 = (input, joltsToMeasure) => {
   const sortedAdapterJoltages = parseInput(input, Number).sort((a, b) => a - b);
   const deviceJoltage = sortedAdapterJoltages[sortedAdapterJoltages.length - 1] + 3;
@@ -21,4 +20,32 @@ export const problem1 = (input, joltsToMeasure) => {
   return joltsToMeasure.reduce(
     (acc, jolt) => (joltageDiffs.has(jolt) ? joltageDiffs.get(jolt) * (acc || 1) : acc), 0,
   );
+};
+
+const getCombinations = (input, memo = {}) => {
+  const key = input.join(',');
+  if (key in memo) {
+    return memo[input];
+  }
+  let result = 1;
+  for (let i = 0; i < input.length - 1; i += 1) {
+    if (input[i + 1] - input[i - 1] <= 3) {
+      const newInput = [input[i - 1]].concat(input.slice(i + 1));
+      result += getCombinations(newInput, memo);
+    }
+  }
+
+  // eslint-disable-next-line no-param-reassign
+  memo[key] = result;
+
+  return result;
+};
+
+export const problem2 = (input) => {
+  const sortedAdapterJoltages = parseInput(input, Number).sort((a, b) => a - b);
+  const deviceJoltage = sortedAdapterJoltages[sortedAdapterJoltages.length - 1] + 3;
+  sortedAdapterJoltages.push(deviceJoltage);
+  sortedAdapterJoltages.unshift(0);
+
+  return getCombinations(sortedAdapterJoltages);
 };
--- day-10/solution.test.js
+++ day-10/solution.test.js
@@ -1,11 +1,18 @@
 import { INPUT } from './__fixtures__/input';
 // import { INPUT } from './input';
 
-import { problem1 } from './solution';
+import { problem1, problem2 } from './solution';
 
 describe('Day 10 - Part One', () => {
   it('should return correct answer with input', () => {
     const result = problem1(INPUT, [1, 3]);
     expect(result).toBe(35);
+  });
+});
+
+describe('Day 10 - Part Two', () => {
+  it('should return correct answer with input', () => {
+    const result = problem2(INPUT);
+    expect(result).toBe(8);
   });
 });
